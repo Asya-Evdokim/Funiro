a25e8c089f2d505ce7f138e4dc3424be37d35812
Анастасия Евдокимова <anastasiaevdokimova@MacBook-Pro-Anastasia.local>
2021-08-06 16:58:57 +0300

    adaptiv-footer

--- src/components/base/Footer.vue
+++ src/components/base/Footer.vue
@@ -1,6 +1,6 @@
 <template>
   <footer class="footer">
-    <div class="footer__container container">
+    <div class="footer__container container grid">
       <div class="footer__main">
         <a href="#" class="footer__logo title-footer">Funiro.</a>
         <p class="footer__text">Worldwide furniture store since 2020. 
@@ -14,31 +14,31 @@
       <div class="footer__menu menu-footer">
         <nav class="menu-footer__body">
           <ul class="menu-footer__list">
-            <li class="menu-footer__item">
+            <li class="menu-footer__item" @click="toggleSpoiler" :class="{active: menuSpoiler.spoiler}">
               <a href="#" class="menu-footer__link title-footer">Menu</a>
               <button class="menu-footer__arrow icon-arrow-down"></button>
               <ul class="menu-footer__sub-list">
-                <li class="menu-footer__sub-item"><a href="#" class="menu-footer__sub-item">Products</a></li>
+                <li class="menu-footer__sub-item" ><a href="#" class="menu-footer__sub-item">Products</a></li>
                 <li class="menu-footer__sub-item"><a href="#" class="menu-footer__sub-item">Rooms</a></li>
                 <li class="menu-footer__sub-item"><a href="#" class="menu-footer__sub-item">Inspirations</a></li>
                 <li class="menu-footer__sub-item"><a href="#" class="menu-footer__sub-item">About Us</a></li>
                 <li class="menu-footer__sub-item"><a href="#" class="menu-footer__sub-item">Terms & Policy</a></li>
               </ul>
               </li>
-            <li class="menu-footer__item">
+            <li class="menu-footer__item" @click="toggleSpoiler" :class="{active: menuSpoiler.spoiler}">
               <a href="#" class="menu-footer__link title-footer">Account</a>
               <button class="menu-footer__arrow icon-arrow-down"></button>
-              <ul class="menu-footer__sub-list">
+              <ul class="menu-footer__sub-list" :class="{active: menuSpoiler.spoiler}">
                 <li class="menu-footer__sub-item"><a href="#" class="menu-footer__sub-item">My Account</a></li>
                 <li class="menu-footer__sub-item"><a href="#" class="menu-footer__sub-item">Checkout</a></li>
                 <li class="menu-footer__sub-item"><a href="#" class="menu-footer__sub-item">My Cart</a></li>
                 <li class="menu-footer__sub-item"><a href="#" class="menu-footer__sub-item">My catalog</a></li>
               </ul>
               </li>
-            <li class="menu-footer__item">
+            <li class="menu-footer__item" @click="toggleSpoiler" :class="{active: menuSpoiler.spoiler}">
               <a href="#" class="menu-footer__link title-footer">Stay Connected</a>
               <button class="menu-footer__arrow icon-arrow-down"></button>
-              <ul class="menu-footer__sub-list">
+              <ul class="menu-footer__sub-list" :class="{active: menuSpoiler.spoiler}">
                 <li class="menu-footer__sub-item"><a href="https://www.facebook.com/" class="menu-footer__sub-item">Facebook</a></li>
                 <li class="menu-footer__sub-item"><a href="https://www.instagram.com/" class="menu-footer__sub-item">Instagram</a></li>
                 <li class="menu-footer__sub-item"><a href="https://twitter.com/" class="menu-footer__sub-item">Twitter</a></li>
@@ -65,9 +65,216 @@ export default {
   name: "Footer",
   components: {
     },
+
+    data() {
+      return {
+        menuSpoiler: {
+          spoiler: false
+        }
+    }
+},
+
+methods: {
+  toggleSpoiler() {
+        this.menuSpoiler.spoiler = !this.menuSpoiler.spoiler;
+      }
+}
 }
 </script>
 
 <style scoped lang="scss">
+.footer {
 
+		&__container {
+      display: grid;
+      border-top: 1px solid #D8D8D8;
+      padding-top: clamp(20px, 6vw, 80px);
+      padding-bottom: clamp(20px, 6vw, 50px);
+
+      @include respond-to('lg') {
+        grid-template-rows: auto auto;
+        grid-template-columns: 1fr 290px;
+      }
+
+      @include respond-to('sm') {
+        display: block;
+      }
+		}
+
+		&__main {
+      grid-column: 1/4;
+      display: grid;
+      grid-gap: clamp(5px,1vw,16px);
+
+      @include respond-to('lg') {
+        grid-column: 2 span;
+      }
+		}
+
+		&__text {
+      color: $color-gray;
+      padding-right: 50px;
+		}
+
+		&__menu {
+      grid-column: 4/10;
+
+      @include respond-to('lg') {
+        grid-column: auto;
+      }
+
+      @include respond-to('sm') {
+        padding: 20px 0;
+      }
+		}
+
+		&__subscribe {
+      grid-column: 10/13;
+
+      @include respond-to('lg') {
+        grid-column: auto;
+      }
+		}
+}
+
+.title-footer {
+  font-size: clamp(20px, 3vw, 24px);
+  line-height: 120%;
+  font-weight: 700;
+  color: #000;
+}
+
+.contacts-footer {
+  display: grid;
+  grid-gap: clamp(10px,1vw,16px);
+
+		&__item {
+      display: flex;
+      align-items: baseline;
+      color: $color-gray;
+      transition: color 0.3s ease 0s;
+
+      &::before {
+       padding-right: 8px;
+       font-size: 20px;
+      }
+
+      @media (any-hover: hover) {
+        &:hover {
+          color: $color-orange;
+        }
+      }
+		}
+}
+
+.menu-footer {
+
+		&__list {
+      display: flex;
+
+      @include respond-to('xs') {
+        flex-direction: column;
+
+      }
+		}
+
+    &__link {
+
+      @include respond-to('xs') {
+        flex: 1;
+
+      }
+    }
+
+		&__item {
+      flex: 0 1 30.3333%;
+
+      &:not(:last-child) {
+        padding-left: 13px;
+        
+        @include respond-to('xs') {
+          padding-left: 0;
+          padding-bottom: 20px;
+        }
+      }
+
+      @include respond-to('xs') {
+          position: relative;
+        }
+
+        &.active {
+          .menu-footer__sub-list {
+            display: list-item;
+          }
+        }
+		}
+
+		&__arrow {
+      display: none;
+
+      @include respond-to('xs') {
+        display: block;
+        position: absolute;
+        top: 0;
+        right: 0;
+      }
+		}
+
+    &__sub-list {
+      @include respond-to('xs') {
+        display: none;       
+      }
+    }
+
+		&__sub-item {
+      color: $color-gray;
+      padding-top: clamp(10px, 2vw, 16px);
+
+      @media (any-hover: hover) {
+        &:hover {
+          color: $color-orange;
+        }
+      }
+
+      
+		}
+}
+
+.subscribe {
+
+  display: flex;
+  flex-direction: column;
+
+		&__title {
+      padding-bottom: clamp(10px, 2vw, 16px);
+		}
+
+		&__form {
+      display: flex;
+      padding-right: 4px;
+		}
+
+		&__input {
+      flex: 1;
+      border: none;
+      background-color: #F4F5F7;
+      padding: 10px 12px;
+		}
+
+		&__button {
+      flex: 0 0 45px;
+      height: 45px;
+      font-size: 20px;
+      background-color: $color-orange;
+      color: #fff;
+      margin-left: 4px;
+      transition: background-color 0.3s ease 0s;
+
+      @media (any-hover: hover) {
+        &:hover {
+          background-color:#e2b396;
+        }
+      }
+		}
+}
 </style>
